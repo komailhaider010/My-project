95c35d92f46d493450596994bbf090aee7aa1b85
Komail Haider <komailhaider010@gmail.com>
2023-07-21 13:25:59 +0500

    user profile image upload sucessfully

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -79,7 +79,9 @@ const handleDelete = async (id)=>{
           <div key={element._id}className="commentCardBox">
           
           <div className="CommentHeading">
-            <h4 className="userName">{element.userId.username}</h4>
+          <img src={`http://localhost:8000/${element.userId.profileImg}`} alt="ProfileIMG" className='userProfileImg'/>
+            <span className="userName">{element.userId.username}</span>
+            
             <h4 className="topicName">{element.blogTitle}</h4>
             <img src={`http://localhost:8000/${element.blogImg}`} alt="Blog Image" className='blogImag' />
           </div>
--- frontend/src/pages/userProfile.jsx
+++ frontend/src/pages/userProfile.jsx
@@ -7,7 +7,22 @@ import { useEffect } from 'react';
 const UserProfile = () => {
     const {userId} = useParams();
     const [user, setUser] = useState('')
+    const [profileImg, setProfileImg] = useState();
 
+const updateUserProfile = async ()=>{
+  try {
+    const formData = new FormData();
+    formData.append('profileImg', profileImg);
+    
+    const response = await axios.patch(`http://localhost:8000/updateuser/${userId}`, formData);
+    window.alert("Profile Data Updated Sucessfully");
+    setProfileImg();
+    
+  } catch (error) {
+    window.alert(error);
+  }
+
+}
     const getUser = async () => {
         try {
           const response = await axios.get(`http://localhost:8000/home/${userId}`);
@@ -19,7 +34,7 @@ const UserProfile = () => {
          };
     useEffect(() => {
     getUser();
-    }, [])
+    }, [updateUserProfile])
     
 
   return (
@@ -27,8 +42,16 @@ const UserProfile = () => {
     <Navbar   userId={userId}/>
 
     <div className="userProfileMainBox">
+        <img src={`http://localhost:8000/${user.profileImg}`} alt="ProfileIMG" className='userProfileImg'/>
         <h4>{user.username}</h4>
         <h5>{user.email}</h5>
+
+        <input type="file"
+         name='profileImg' id='ProfileImg'
+            onChange={(e)=>setProfileImg(e.target.files[0])}
+                />
+
+            <button onClick={updateUserProfile}>Update</button>
     </div>
     
     </>
